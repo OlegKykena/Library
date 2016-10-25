db4eec34c6b0710bc6037e76a2d9112c563508f8
Oleg Kykena <ikena7@gmail.com>
2016-10-25 21:23:30 +0300

    Kykena. Add CRUD-operations in service

--- src/main/java/com/softserve/controller/IndexController.java
+++ src/main/java/com/softserve/controller/IndexController.java
@@ -5,13 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.softserve.service.AuthorService;
 import com.softserve.service.BookService;
+import com.softserve.service.GenreService;
 
 @Controller
 public class IndexController {
 	
 	@Autowired
 	private BookService bookService;
+	
+	@Autowired
+	private AuthorService authorService;
+	
+	@Autowired
+	private GenreService genreService;
 
 	@RequestMapping("/")
 	public String showIndex(){
@@ -22,5 +30,17 @@ public class IndexController {
 	public String showBooks(Model model) {
 		model.addAttribute("books" , bookService.findAvailableBooks());
 		return "books";
+	}
+	
+	@RequestMapping("/authors")
+	public String showAuthors(Model model) {
+		model.addAttribute("authors" , bookService.findAvailableBooks());
+		return "authors";
+	}
+	
+	@RequestMapping("/genres")
+	public String showGenres(Model model) {
+		model.addAttribute("genres" , genreService.getListSortByAlh());
+		return "genres";
 	}
 }
